61ee2ffedf189370d66a1a128cbf6c7059d45a3d
Artur Maia <artur.novazzi@outlook.com>
2026-04-06 21:49:16 -0300

    feat: arrumando gramática

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,8 @@ let tipoVinho = prompt('Insria o tipo do vinho \n-Tinto \n-Branco \n-Rose');
 let safraVinho = prompt('Insira a safra do vinho (ano de produção)');
 let quantidade = prompt('Insira a quantidade em estoque');
 
+alert('cadastro de vinho realizado, confira as informações no console')
+
 console.log("╔══════════════════════════════════════╗");
 console.log("║   🍷  VINHARIA AGNELLO – ESTOQUE     ║");
 console.log("╠══════════════════════════════════════╣");
